d06c9d04816eb0936e19787baafea3d809177e32
Ambre hp <ambre.haphuoc@gmail.com>
2024-05-31 18:30:08 +0200

    ajout du random header

--- front-page.php
+++ front-page.php
@@ -11,8 +11,36 @@ get_header();
 ?>
 <div id="front-page">
     <section id="content">
-        <!-- Chargement du hero -->
-        <?php get_template_part('template-parts/header/hero'); ?>
+        <!-- Intégration du random hero -->
+        <!-- Wrap Slider Area -->
+        <div class="hero-area">
+            <div class="hero-thumbnail">
+                <!-- Initialisation de post à afficher -->
+                <?php
+                $custom_args = array(
+                    'post_type' => 'photo',
+                    'orderby'   => 'rand',
+                    'posts_per_page' => 1,
+                );
+                //On crée ensuite une instance de requête WP_Query basée sur les critères placés dans la variables $args
+                $query_hero = new WP_Query($custom_args);
+                ?>
+                <!-- Récupération d'un post photo en mode aléatoire (rand) -->
+                <?php while ($query_hero->have_posts()) : ?>
+                    <?php $query_hero->the_post(); ?>
+
+                    <?php if (has_post_thumbnail()) : ?>
+                        <a href="<?php the_permalink(); ?>" alt="<?php the_title(); ?>"><?php the_post_thumbnail('hero'); ?></a>
+                    <?php endif; ?>
+
+                <?php endwhile; ?>
+                <h1 class="title-hero">Photographe event</h1>
+            </div>
+        </div>
+        <?php
+        // On réinitialise à la requête principale
+        wp_reset_postdata();
+        ?>
 
         <!-- Chargement des filtres -->
         <?php get_template_part('template-parts/post/photo-filter'); ?>
