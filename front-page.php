9f4b36d650e6107ce965f9e768325cbcaade5442
Ambre hp <ambre.haphuoc@gmail.com>
2024-06-04 19:21:02 +0200

    Nettoyage code

--- front-page.php
+++ front-page.php
@@ -158,7 +158,32 @@ get_header();
                 </form>
                 <!-- On parcourt chacun des articles résultant de la requête -->
                 <?php while ($query->have_posts()) : $query->the_post();
-                    get_template_part('template-parts/publication');
+                    if (has_post_thumbnail()) : ?>
+
+                        <?php
+                        // On récupère la taxonomie ACF actuelle
+                        $term = get_queried_object();
+                        // On déclare les variables pour les informations de la photo
+                        $reference = get_field('reference');
+                        $categorie  = my_acf_load_value('name', get_field('categorie-acf'));
+                        ?>
+
+                        <!-- On génère le nombre de photo en fonction de l'option dans WordPress -->
+                        <div class="news-info brightness">
+                            <p class="photo-reference"><?php echo $reference; ?></p>
+                            <h3 class="info-tax"><?php echo $categorie; ?></h3>
+                            <a href="<?php the_permalink() ?>" aria-label="Voir le détail de la photo <?php the_title(); ?>" alt="<?php the_title(); ?>" title="Voir le détail de la photo"><span class="detail-photo"></span></a>
+                            <?php the_post_thumbnail(); ?>
+                            <p><?php the_terms($post->ID, 'categorie-acf', ''); ?></p>
+                            <form>
+                                <input type="hidden" name="postid" class="postid" value="<?php the_id(); ?>">
+
+                                <a class="openLightbox" title="Afficher la photo en plein écran" alt="Afficher la photo en plein écran" data-postid="<?php echo get_the_id(); ?>" data-arrow="true">
+                                </a>
+                            </form>
+                        </div>
+
+                <?php endif;
                 endwhile;
                 ?>
             </article>
@@ -178,7 +203,7 @@ get_header();
                 </div>
             </div>
         <?php else : ?>
-            <p>Désolé. Aucun article ne correspond à cette demande.</p>
+            <p>Désolé. Aucun article ne correspond à la demande.</p>
 
         <?php endif; ?>
 
